79b3ee1e1e3cedf4425aee55485d3d31067a53d6
Hao <har@mailbox.org>
2022-12-20 12:34:03 +1100

    Initial Commit

---
+++ Bob/BobApp.swift
@@ -0,0 +1,17 @@
+//
+//  BobApp.swift
+//  Bob
+//
+//  Created by Hao on 20/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct BobApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bob/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Bob
+//
+//  Created by Hao on 20/12/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
